fc0e5511a576fd78cf4887af51c913a24752e9c9
junhyung <wnsgud0895@gmail.com>
2024-03-29 16:23:16 +0900

    wip: profilePage CSS 작업중

---
+++ Front/src/components/profile/ProfilePageMain.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ProfilePageMain = styled.div`
+  display: flex;
+`
+
--- Front/src/pages/user/ProfilePage.tsx
+++ Front/src/pages/user/ProfilePage.tsx
@@ -1,28 +1,32 @@
-import { useSelector } from "react-redux";
-import MenuBar from "../../components/profile/MenuBar";
-import PlaylistBox from "../../components/profile/playlist/PlaylistBox";
-import { RootState } from "../../stores/store";
-import ColorLine from "../../components/profile/ColorLine";
-
+import { useSelector } from 'react-redux';
+import MenuBar from '../../components/profile/MenuBar';
+import PlaylistBox from '../../components/profile/playlist/PlaylistBox';
+import { RootState } from '../../stores/store';
+import ColorLine from '../../components/profile/ColorLine';
+import { ProfilePageMain } from '../../components/profile/ProfilePageMain';
 
 
 function ProfilePage() {
-  const selectedMenu = useSelector((state: RootState) => state.menu.selectedMenu);
+  const selectedMenu = useSelector(
+    (state: RootState) => state.menu.selectedMenu,
+  );
 
   let ComponentToShow;
   switch (selectedMenu) {
-    case "플레이리스트":
+    case '플레이리스트':
       ComponentToShow = PlaylistBox;
       break;
   }
-  
+
   return (
     <>
-    <ColorLine/>
-    <MenuBar />
-    {ComponentToShow && <ComponentToShow />}
+      <ColorLine />
+      <ProfilePageMain>
+          <MenuBar />
+          {ComponentToShow && <ComponentToShow />}
+      </ProfilePageMain>
     </>
-  )
+  );
 }
 
 export default ProfilePage;
